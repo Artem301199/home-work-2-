71729cfb372bf185150da9178023c66051eb56dd
Artem <artem-vishnevskiy-00@mail.ru>
2022-08-26 15:27:56 +0300

    задача 3

---
+++ home_3/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Введите ваще число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+switch(n)
+{
+    case 1 :
+    {
+        Console.WriteLine("нет");
+    }
+    break;
+    
+    case 2:
+    {
+        Console.WriteLine("нет");
+    }
+    break;
+    
+    case 3:
+    {
+        Console.WriteLine("нет");
+    }
+    break;
+
+    case 4:
+    {
+        Console.WriteLine("нет");
+    }
+    break;
+
+    case 5:
+    {
+        Console.WriteLine("нет");
+    }
+    break;
+
+    case 6:
+    {
+        Console.WriteLine("выходной");
+    }
+    break;
+    
+    case 7:
+    {
+        Console.WriteLine("выходной");
+    }
+    break;
+
+}
+
+
